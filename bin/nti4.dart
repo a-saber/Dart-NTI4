5755007990f1fe627518ce5a8cc538c4f95ffe20
a-saber <ahs629862@gmail.com>
2025-10-17 12:15:38 +0300

    gocery 3 cases

--- bin/nti4.dart
+++ bin/nti4.dart
@@ -4,22 +4,102 @@ void main()
 {
   List<Map<String, Map<String, dynamic>>> invoices = [];
   List<Map<String, dynamic>> products = [];
-  displayOptions();
-  int input = handleInput((String input)
-  {
-    int value= int.tryParse(input)??0;
-    return (value >=1 && value <=6)? value : null;    
-  });
+  while(true){
+    displayOptions();
+    int option = handleInput(
+      prompt: "Enter Option Number (1-6):",
+      casting: (String input)
+      {
+        int value= int.tryParse(input)??0;
+        return (value >=1 && value <=6)? value : null;    
+      }
+    );
 
-  String name = handleInput();
-  double price = handleInput((String input){
-    double value = double.tryParse(input)??0;
-    return (value>0) ? value : null;
-  });
-  print(input);
+    switch (option) {
+      case 1:
+        // Add New Product
+        addProduct(products);
+
+      case 2:
+      // show products with quantity > 0
+      for(var product in products)
+      {
+        if(product['quantity'] > 0)
+        {
+          print("${product['name']} \$${product['price']} , Qty: ${product['quantity']}");
+        }
+      }
+
+      case 3:
+      // update product
+      updateProduct(products);
+
+
+
+      case 6:
+        // Exit
+        print('Exiting Inventory Management System. Goodbye!');
+        return;
+    } 
+  }
+
+
+  // String name = handleInput();
+  // double price = handleInput((String input){
+  //   double value = double.tryParse(input)??0;
+  //   return (value>0) ? value : null;
+  // });
+  // print(input);
 
 }
-
+void updateProduct(List<Map<String, dynamic>> products)
+{
+      // input produt index
+    int productIndex =handleInput(
+      prompt: "Enter Product Number",
+      casting: (String input)
+      {
+        int value = int.tryParse(input)??-1;
+        return (value >=0 && value < products.length)? value-1 : null;
+      }
+    );
+      
+          int productQuantity = handleInput(
+        prompt: "Enter Product Quantity:",
+        casting: (String input)
+        {
+          int value = int.tryParse(input)??0;
+          return (value>0) ? value : null;
+        }
+      );
+      products[productIndex]['quantity'] = productQuantity;
+}
+void addProduct(List<Map<String, dynamic>> products)
+{
+      String productName = handleInput(prompt: "Enter Product Name:");
+    double productPrice = handleInput(
+      prompt: "Enter Product Price:",
+      casting: (String input)
+      {
+        double value = double.tryParse(input)??0;
+        return (value>0) ? value : null;
+      }
+    );
+    int productQuantity = handleInput(
+      prompt: "Enter Product Quantity:",
+      casting: (String input)
+      {
+        int value = int.tryParse(input)??0;
+        return (value>0) ? value : null;
+      }
+    );
+    // add to products list
+    products.add({
+      'name': productName,
+      'price': productPrice,
+      'quantity': productQuantity
+    });
+}
 void displayOptions()
 {
   print('''
@@ -32,12 +112,13 @@ Options:
 4. Purchase Products
 5. Show Invoices
 6. Exit
-Please choose an option:
+
 ''');
 }
 
 
-dynamic handleInput([dynamic Function(String)? casting]){
+dynamic handleInput({String prompt = "Enter Value:", dynamic Function(String)? casting}) {
+  print(prompt);
   while(true){
     String input = stdin.readLineSync()??'';
     if(input.isEmpty){
